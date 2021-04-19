11658dbf0d3a4618eed98e6089cfc35ff6d4b624
AryanAhire <66762638+sasukeuchiha-clan@users.noreply.github.com>
2021-04-19 17:01:13 +0530

    Create ssh.sh

---
+++ ssh.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+tg(){
+	bot_api=1692865707:AAHvKZogI5sUGtqjd2sZvNl8tib0Q_xcYkY # Your tg bot api, dont use my one haha, it's better to encrypt bot api too.
+	your_telegram_id=$1 # No need to touch 
+	msg=$2 # No need to touch
+	curl -s "https://api.telegram.org/bot${bot_api}/sendmessage" --data "text=$msg&chat_id=${your_telegram_id}"
+}
+
+id=571213272 # Your telegram id
+
+tmate -S /tmp/tmate.sock new-session -d && tmate -S /tmp/tmate.sock wait tmate-ready && send_shell=$(tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}') && tg $id "Build Failed" && tg $id "$send_shell"
